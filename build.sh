86a8c582d0af1caa99af038b99e0cfe635fd2cfa
Kyusik Shin <banaconda.shin@gmail.com>
2023-04-22 16:51:06 +0000

    change prerequisite and add output name

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ virtualenv venv
 
 source venv/bin/activate
 
+#pip3 uninstall crypto
+#pip3 uninstall pycrypto
+#pip3 install pycryptodome
 #pip install http://libguestfs.org/download/python/guestfs-1.40.2.tar.gz
 
-pyinstaller -F main.py
+pyinstaller -F main.py -n image_maker
 
+deactivate
 
 rm -rf build
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!venv/bin/python3
 import json
 import sys
 from volume import *
